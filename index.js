434de64a4b5b04e09f6b1581de5cc1a5003b006f
Osakwe Anthony Abuchi <64107814+void-fx@users.noreply.github.com>
2020-10-21 23:04:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,36 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-}
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-function handleClick() {
-  var buttonInnerHTML = this.innerHTML;
-  makesound(buttonInnerHTML);
-  buttonAnimation(buttonInnerHTML);
-}
+var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-document.addEventListener("keydown", function(event){
-  makesound(event.key);
-  buttonAnimation(event.key);
-});
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
 
 
-function makesound(key) {
-  switch (key) {
-    case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-    case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "d":
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    case "j":
-      var snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
-    case "k":
-      var kick = new Audio("sounds/kick-bass.mp3");
-      kick.play();
-      break;
-    case "l":
-      var crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-    default:
-    console.log(buttonInnerHTML);
-}
-}
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-function buttonAnimation(currentkey)
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2)
 {
-var activeButton =  document.querySelector("." + currentkey);
-activeButton.classList.add("pressed");
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
-  }, 200);
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  document.querySelector( ".p1").innerHTML = " ✔";
+  document.querySelector( ".p2").innerHTML = " ❌";
+
+}
+else if (randomNumber1 < randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+  document.querySelector(".p2").innerHTML = "✔";
+  document.querySelector( ".p1").innerHTML = "❌";
+
+}
+else
+ {
+    document.querySelector("h1").innerHTML = "Draw!"
 }
